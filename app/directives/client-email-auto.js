0ae31042d5b24256cba8c2ff70961a005b317130
salmaniz82 <salmaniz.82@gmail.com>
2018-11-19 21:28:19 +0500

    autocomplete list fetch on click fixed with api

--- app/directives/client-email-auto.js
+++ app/directives/client-email-auto.js
@@ -7,21 +7,35 @@
             restrict: 'C',
             link: function(scope, ele, attr, ngModel)
             {
-                var url = API_URL+'/api/clients/civilno';
 
+
+                ele.bind('click', function(e) {
+
+               var url = API_URL+'/api/clients/civilno';
                 $http.get(url).then(function(response){
                    var data = response.data;
 
-                    ele.autocomplete({
-                        data: data,
-                        limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
-                        onAutocomplete: function(val) {
-                            // Callback function when value is autcompleted.
-                        },
-                        minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+                        ele.autocomplete({
+                            data: data,
+                            limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+                            onAutocomplete: function(val) {
+                                // Callback function when value is autcompleted.
+                            },
+                            minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+                        });
+
                     });
 
+
+                    
+
                 });
+
+                
+
+                
+
+                
 
 
             }
